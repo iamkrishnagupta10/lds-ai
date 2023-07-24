da98b2848e925f6bca3d52361ebe8cc3c9f002e1
Krishna Gupta <129900110+iamkrishnagupta10@users.noreply.github.com>
2023-07-24 13:29:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import requests
 
-API_URL = "https://lds-ai.onrender.com/api/v1/prediction/4fffec95-202e-4fbd-ad00-c8125a879644"
+API_URL = "https://lds-ai.onrender.com/api/v1/prediction/4f13ac2f-d3f9-4114-8e54-e036fb321212"
 
 def query(payload):
     response = requests.post(API_URL, json=payload)
     return response.json()
 
-st.title('LDS Ai Helper Bot')
+st.title('LDS Therapist')
 
 message = st.text_input('Enter your message')
 
